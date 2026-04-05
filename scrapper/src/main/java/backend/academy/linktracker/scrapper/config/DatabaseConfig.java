c0fda5ccc7e8660636888c50d40d33096a650775
VMExite <vadimmalkov26@gmail.com>
2026-04-05 17:43:46 +0300

    feat: move jpa configuration to application.yaml, now not use Java Config for Datasource configuration

--- scrapper/src/main/java/backend/academy/linktracker/scrapper/config/DatabaseConfig.java
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/config/DatabaseConfig.java
@@ -1,13 +1,11 @@
 package backend.academy.linktracker.scrapper.config;
 
-import backend.academy.linktracker.scrapper.properties.DatabaseProperties;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -15,13 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @RequiredArgsConstructor
 public class DatabaseConfig {
-    private final DatabaseProperties properties;
-
-    @Bean
-    public DataSource dataSource() {
-        return new DriverManagerDataSource(properties.getUrl(), properties.getUsername(), properties.getPassword());
-    }
-
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
